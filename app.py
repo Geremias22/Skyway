c12405c073d90d0668ef31a11674f0d36e7b6629
elgabarba <elgabarba@ceroca.cat>
2025-05-07 19:44:06 +0200

    front-end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from controllers.generador_vuelos import buscar_vuelos
 
 app = Flask(__name__)
@@ -12,5 +12,13 @@ def mostrar_vuelos():
     vuelos = buscar_vuelos()  # Esta función ya prepara y devuelve los vuelos procesados
     return render_template("resultados_vuelos.html", vuelos=vuelos, origen=vuelos[0]["salida"], destino=vuelos[0]["llegada"])
 
+@app.route('/buscar', methods=['POST'])
+def procesar_busqueda():
+    # Obtener los datos del formulario
+    lugar = request.form.get('location')
+    num_personas = request.form.get('days')
+    fecha = request.form.get('date')
+    print(lugar, num_personas, fecha)
+    
 if __name__ == "__main__":
     app.run(debug=True)
